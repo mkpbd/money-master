03f53dd545a391947ea813c5bafa76243d4bd23f
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-17 20:50:33 +0600

    input validation

--- js/app.js
+++ js/app.js
@@ -1,118 +1,210 @@
-// income and expanse  handaler 
-document.getElementById('calculate-expense').addEventListener('click', function(event){
-
+// income and expanse  handaler
+document
+  .getElementById("calculate-expense")
+  .addEventListener("click", function (event) {
     console.log(event);
 
-        const income = getInputValeAndParse('income');
-        const food = getInputValeAndParse('food');
-        const rent = getInputValeAndParse('rent');
-        const cloth = getInputValeAndParse('cloth');
+    /*================ get Data form inut flields =============*/
+    const income = getInputValeAndParse("income");
+    const food = getInputValeAndParse("food");
+    const rent = getInputValeAndParse("rent");
+    const cloth = getInputValeAndParse("cloth");
 
-       // debugger;
-        if(income <= 0 || isNaN(income)){
-            errorMessage('income-error-success', 'income only positeve value allow ??', 'block');
-            //return;
+    // debugger;
+
+    /* ============== Income  Validation  ===================*/
+    if (isNaN(income)) {
+      errorMessage(
+        "income-error-success",
+        " string are not  allow ??",
+        "block"
+      );
+      //return;
+    } else if (income <= 0) {
+      errorMessage(
+        "income-error-success",
+        "Plase Give Valid Income   ??",
+        "block"
+      );
+      //return;
+    } else {
+
+      errorMessage("income-error-success", "", "none");
+
+    }
+    /* ============== Food Validation  ===================*/
+    if (isNaN(food)) {
+
+         errorMessage("food-error-success", "String are not allow ??", "block");
+
+    } else if (food < 0) {
+
+        errorMessage(
+            "food-error-success",
+            "food can not nagative Value allow ??",
+            "block"
+        );
+
+    } else {
+         errorMessage("food-error-success", "", "none");
+    }
+
+
+    /* ============== rent Validation  ===================*/
+        if (rent < 0) {
+        errorMessage(
+            "rent-error-success",
+            "rent can not nagative Value allow ??",
+            "block"
+        );
+        } else if (isNaN(rent)) {
+             errorMessage("rent-error-success", "Plase Give Valid Number", "block");
+        } else {
+              errorMessage("rent-error-success", "", "none");
         }
-        
-        if( food <0 || isNaN(food)){
-           
-           errorMessage('food-error-success', 'food can not nagative Value allow ??', 'block');
-            
-        }
-        
-        if( rent <0 || isNaN(rent)){
-           
-           errorMessage('rent-error-success', 'rent can not nagative Value allow ??', 'block');
-            
-        }
-        
-        if( cloth <0 || isNaN(cloth)){
-           
-           errorMessage('cloth-error-success', 'cloth can not nagative value  allow ??', 'block');
-            
-        }
-        
-        if(income <= 0 || food <0 || rent < 0 || cloth < 0 ) return;
-        if(isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(cloth) ) return;
 
-        const totalExpense = food + rent + cloth;
-        const balance = income - totalExpense;
 
-      //  console.log('Balance = ', balance, " total Expense = ",totalExpense);
 
-      getTestContentShowAndReturnValue('total-expense' ,totalExpense);
-      getTestContentShowAndReturnValue('total-balance' ,balance);
+    /* ============== Cloth Validation  ===================*/
 
-        //console.log("income = ", income , ' Food = ', food, " rend = ", rent, " cloth = ", cloth);
-});
+    if (isNaN(cloth)) {
+      errorMessage("cloth-error-success", "String  are not Allow ??", "block");
+    } else if (cloth < 0) {
+      errorMessage("cloth-error-success", "Plase Give Valid Numvber", "block");
+    } else {
+      errorMessage("cloth-error-success", "", "none");
+    }
 
-// get input value form input field 
-function getInputValeAndParse(inputId){
+
+    /*====================== cheack all validation data ================*/
+
+    if (income <= 0 || food < 0 || rent < 0 || cloth < 0) return;
+    if (isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(cloth)) return;
+
+    /*================== calculation data =======================*/
+
+            const totalExpense = food + rent + cloth;
+            const balance = income - totalExpense;
+
+    //  console.log('Balance = ', balance, " total Expense = ",totalExpense);
+     /* =========================== Check your balance and total Expanse ================== */
+            if (totalExpense > income) {
+            errorMessage(
+                "balance-error-success",
+                "Your Income Less = " +
+                income +
+                " then Totel Expense =  " +
+                totalExpense,
+                "block"
+            );
+
+            return;
+            } else {
+            errorMessage("balance-error-success", "", "none");
+            }
+
+            /*====================== Show Data ===============*/
+                setTextContentValue("total-expense", totalExpense);
+                setTextContentValue("total-balance", balance);
+
+    //console.log("income = ", income , ' Food = ', food, " rend = ", rent, " cloth = ", cloth);
+  });
+
+
+
+
+// get input value form input field
+function getInputValeAndParse(inputId) {
 
     const getInput = document.getElementById(inputId);
     const parseInput = parseFloat(getInput.value);
-
     return parseInput;
 
 }
 
-// show error message function 
 
-function errorMessage (errorId , errorText, showMessage){
 
+// show error message function
+
+function errorMessage(errorId, errorText, showMessage) {
+ 
     const getErrorId = document.getElementById(errorId);
-    getErrorId.classList.add('invalid-feedback')
+    getErrorId.classList.add("invalid-feedback");
     getErrorId.style.display = showMessage;
     getErrorId.innerText = errorText;
-    
-    
+
 }
 
-// get TestContent value sate and get value from text content 
 
-function getTestContentShowAndReturnValue(testContentId, showText){
+
+// get Inner Text from Text Content
+function getTextConvetValue(testContentId) {
 
     const getText = document.getElementById(testContentId);
+    const paresTextContent = parseFloat(getText.innerText);
+    return paresTextContent;
 
-    if(showText >= 0){
-        getText.innerText = showText;
-    }else{
-        const paresTextContent = parseFloat(getText.innerText);
-
-        return paresTextContent;
-    }
-    
-
-   
 }
 
 
 
-// saving calculation 
+// set innerText from Text Content
 
-document.getElementById('save-btn').addEventListener('click',function(event){
+function setTextContentValue(testContentId, showText) {
 
-    // console.log(event);
+    const getText = document.getElementById(testContentId);
+    getText.innerText = showText;
 
-    const saving = getInputValeAndParse('save');
-    const balance = getTestContentShowAndReturnValue('total-balance', 0);
+}
 
-   // console.log("saving ", saving, " balance = ", balance);
 
-   const   savingInParsen = balance * saving /100;
-   const remaingBalance = balance - savingInParsen;
-    if(balance<savingInParsen){
-        errorMessage('saving-error-success', 'You can not saving  more than balance = '+balance, 'block');
-        return;
-    }else{
-        errorMessage('saving-error-success', '', 'none');
-    }
+
+// saving calculation
+
+document.getElementById("save-btn").addEventListener("click", function (event) {
+        // console.log(event);
+
+        const saving = getInputValeAndParse("save");
+        const balance = getTextConvetValue("total-balance");
+
+        // console.log("saving ", saving, " balance = ", balance);
+            /*=============== cheack balance zerro or more  =================== */
+        if (isNaN(balance)) {
+            errorMessage("saving-error-success", "Your balance is not enough", "block");
+            return;
+        }  else {
+            errorMessage("saving-error-success", "", "none");
+        }
+
+        
+        /*=============== input saving file validation =================== */
+        if (isNaN(saving)) {
+            errorMessage("saving-error-success", "String are not allow", "block");
+            return;
+        } else if (saving <= 0) {
+            errorMessage("saving-error-success", "Place Give Valid Number ", "block");
+            return;
+        } else {
+            errorMessage("saving-error-success", "", "none");
+        }
+        /*=============== calculation  savings ============= */
+        const savingInParsen = (balance * saving) / 100;
+        const remaingBalance = balance - savingInParsen;
+
   // console.log(savingInParsen);
 
-  getTestContentShowAndReturnValue('remaing-balance', remaingBalance);
-  getTestContentShowAndReturnValue('saving-amount', savingInParsen);
-
-    
-    
-    
-})
+  /*===========================  balance and saving balance Validation =================== */
+            if (balance < savingInParsen) {
+                errorMessage(
+                "saving-error-success",
+                "You can not saving  more than balance = " + balance,
+                "block"
+                );
+                return;
+            } else {
+                errorMessage("saving-error-success", "", "none");
+            }
+            /*================== show data  ============== */
+            setTextContentValue("remaing-balance", remaingBalance);
+            setTextContentValue("saving-amount", savingInParsen);
+});
